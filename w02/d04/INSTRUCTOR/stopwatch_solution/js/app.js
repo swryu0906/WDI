c61f311c461b6f7d7171b251ced53b1daac440fa
DBrodsky90 <DBrodsky90@gmail.com>
2015-10-08 10:10:11 -0400

    stop watch solution

---
+++ w02/d04/INSTRUCTOR/stopwatch_solution/js/app.js
@@ -0,0 +1,26 @@
+var startTimer = function() {
+	stopWatchHandle = setInterval(function() {
+		var currentTime = Number(document.getElementById('stopwatch').textContent);
+		currentTime++;
+		document.getElementById('stopwatch').textContent = currentTime;
+	}, 1000);
+};
+
+var stopTimer = function() {
+	clearInterval(stopWatchHandle);
+};
+
+var resetTimer = function() {
+	// stopTimer();
+	document.getElementById('stopwatch').innerHTML = "0";
+};
+
+
+window.onload = function() {
+  console.log("The world, like this file, is your oyster.");
+
+  document.getElementById('start').addEventListener('click', startTimer);
+  document.getElementById('stop').addEventListener('click', stopTimer);
+  document.getElementById('reset').addEventListener('click', resetTimer);
+};
+
