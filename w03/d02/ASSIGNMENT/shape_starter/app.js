1fd42482cf9fa871fcf5cad81f4a026b0ae8d1f3
Sungwon Ryu <swryu0906@gmail.com>
2015-10-20 17:49:11 -0400

    Modifies shape_starter files in w03/d02/ASSIGNMENT/shape_starter/

--- w03/d02/ASSIGNMENT/shape_starter/app.js
+++ w03/d02/ASSIGNMENT/shape_starter/app.js
@@ -2,7 +2,23 @@ $(function() {
 	console.log('Loaded, bro');
 });
 
-var makeCircles = function() {
+$('.shpe-box').on('click', '#makeCicles', makeCicles);
+$('.shpe-box').on('click', '#makeSquareds', makeSquares);
+$('.shpe-box').on('click', '#makeCicles', makeCicles);
+
+
+
+var makeCircles = function(num, color) {
+	var container = $(')
+
+	var num = +$(this).closest('.shape-box').find('input[type=number]').val();
+	var color = $(this).closest('.shape-box').find('input[type=text]').val();
+	for(var i = 0; i < num; i++) {
+		var circleDiv = $('<div></div>');
+		circleDiv.addClass('circle');
+		circleDiv.css({'background-color': color});
+		$()
+	}
 
 };
 
@@ -21,3 +37,21 @@ var clearShapes = function(which) {
 var randomColor = function() {
 
 };
+
+$('.shape-box').on('click', '.draw-btn', function() {
+	// console.log('btn is clicked');
+	var num = +$(this).closest('.shape-box').find('input[type=number]').val();
+	var color = $(this).closest('.shape-box').find('input[type=text]').val();
+	// console.log(num + ', ' + shape);
+});
+
+// var ShapeStarter = (function() {
+// 	return {
+// 		init: function() {
+// 				$()
+// 		},
+// 		getUserInput: function {
+//
+// 		}
+// 	};
+// })();
