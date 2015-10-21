f3170715dfde663153eeb5009a9033112662fb2b
Sungwon Ryu <swryu0906@gmail.com>
2015-10-21 03:28:24 -0400

    Modifies multiple_lines() method and adds display_title(), display_main_menu() and subway() methods in w04/d02/homework/subway.js

--- w04/d02/homework/subway.rb
+++ w04/d02/homework/subway.rb
@@ -1,31 +1,3 @@
-# n = [
-#   "Times Square",
-#   "34th",
-#   "28th",
-#   "23rd",
-#   "Union Square",
-#   "8th"
-# ]
-#
-# l = [
-#   "8th Ave",
-#   "6th Ave",
-#   "Union Square",
-#   "3rd Ave",
-#   "1st Ave"
-# ]
-#
-# six = [
-#   "Grand Central",
-#   "33rd",
-#   "28th",
-#   "23rd",
-#   "Union Square",
-#   "Astor Place"
-# ]
-
-
-
 n = ['ts', '34th-n', '28th-n', '23rd-n', 'us']
 l = ['8th', '6th', 'us', '3rd', '1st']
 s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
@@ -35,76 +7,89 @@ o = ["59th", "50th", "ts", "34th-o", "28th-o"]
 @mta[:n] = n
 @mta[:l] = l
 @mta[:s] = s
+@mta[:o] = o
+
+@hash_to_line = {
+  n: "N",
+  l: "L",
+  s: "6",
+  o: "1"
+}
 
 
-
+# display the list of all stops on the given line
 def display_stops(line)
-  # display the list of all stops on the given line
-  puts "Line #{line.upcase} : #{@mta[line.to_sym].join(" - ")}"
+  puts "\nLine #{@hash_to_line[line.to_sym]} : #{@mta[line.to_sym].join(" - ")}"
 end
 
+
+# display the list of lines with their stops on the given lines
 def display_lines(lines)
   lines.each do |line|
     display_stops(line)
   end
 end
 
-# def ask_origin_stop
-#   print "Which stop would you like to get on at : "
-#   origin = gets.chomp
-# end
-#
-# def ask_dest_stop
-#   print "Which stop would you like to get off at : "
-#   dest = gets.chomp
-# end
 
-def ask_stop(stop_type)
-  print "Which stop would you like to get #{stop_type === :origin ? "on" : "off" } at : "
-  stop = gets.chomp.downcase
+# ask a user the line which they like to get on or get off at
+def ask_line(line_type)
+  print "\nWhich line would you like to get #{line_type === :origin ? "on" : "off"} at : "
+  user_response = gets.chomp.upcase
+
+  # when the user input is not correct,
+  # print an error message and ask the same question
+  until @hash_to_line.has_value?(user_response)
+    puts "\nError! You entered a wrong subway line!"
+    print "Which line would you like to get #{line_type === :origin ? "on" : "off"} at : "
+    user_response = gets.chomp.upcase
+  end
+  @hash_to_line.key(user_response)
 end
 
-# def get_stop_index(line, stop)
-#   stop_index = @mta[line.to_sym].index(stop)
-# end
 
-# def get_num_of_stops(info)
-#   origin_index = @mta[info[:origin_line].to_sym].index(info[:origin_stop])
-#   dest_index = @mta[info[:origin_line].to_sym].index(info[:dest_stop])
-#   return (origin_index - dest_index).abs
-# end
+# ask a user the stop which they like to get on or get off at
+def ask_stop(line, stop_type)
+  print "\nWhich stop would you like to get #{stop_type === :origin ? "on" : "off" } at : "
+  user_response = gets.chomp.downcase
 
+  # when the user input is not correct,
+  # print an error message and ask the same question
+  until @mta[line.to_sym].include?(user_response)
+    puts "\nError! You entered a wrong stop name!"
+    print "Which stop would you like to get #{stop_type === :origin ? "on" : "off" } at : "
+    user_response = gets.chomp.downcase
+  end until @mta[line.to_sym].include?(user_response)
+  user_response
+end
+
+
+# get the number of stops for the trip on the same line
 def get_num_of_stops(line, origin_stop, dest_stop)
   origin_index = @mta[line.to_sym].index(origin_stop)
   dest_index = @mta[line.to_sym].index(dest_stop)
   return (origin_index - dest_index).abs
 end
 
-def ask_line(line_type)
-  print "Which line would you like to get #{line_type === :origin ? "on" : "off"} at : "
-  line = gets.chomp.downcase
-end
 
+# display the number of stops result calculated
 def display_result(info)
   if(info[:origin_line] === info[:dest_line])
     num_of_total_stops = get_num_of_stops(info[:origin_line], info[:origin_stop], info[:dest_stop])
-    puts "You need #{num_of_total_stops} stops."
+    puts "\nYour origin stop is #{info[:origin_stop]} and your destination stop is #{info[:dest_stop]}"
+    puts "You need #{num_of_total_stops} stops on #{@hash_to_line[info[:origin_line].to_sym]} line."
   else
     num_of_stops_1 = get_num_of_stops(info[:origin_line], info[:origin_stop], "us")
     num_of_stops_2 = get_num_of_stops(info[:dest_line], info[:dest_stop], "us")
     num_of_total_stops = num_of_stops_1 + num_of_stops_2
 
-    puts "You need #{num_of_stops_1} stops on #{info[:origin_line].upcase} line."
+    puts "\nYou need #{num_of_stops_1} stops on #{info[:origin_line].upcase} line."
     puts "You need to transfer at us."
     puts "You need #{num_of_stops_2} stops on #{info[:dest_line].upcase} line."
     puts "You need total #{num_of_total_stops} stops."
   end
 end
 
-# def get_transfer_stop(info)
-#   case info[:origin_line]
-#
-# end
+
 
 # Commit 1
 # One line functionality
@@ -113,39 +98,28 @@ def one_line(line)
   info = Hash.new
   info[:origin_line] = line
   info[:dest_line] = line
+
   # display the list of all stops on the given line
-
-  # puts "Line #{line.upcase} : #{@mta[line_sym].join(" - ")}"
   display_stops(line)
+
   # ask the stop the user wants to get on at
+  info[:origin_stop] = ask_stop(info[:origin_line], :origin)
 
-  # print "Which stop would you like to get on at : "
-  # origin = gets.chomp
-  info[:origin_stop] = ask_stop(:origin)
-  # origin_index = @mta[line_sym].index(start)
   # ask the stop the user wants to get off at
-
-
-  # print "Which stop would you like to get off at : "
-  # dest = gets.chomp
-  info[:dest_stop] = ask_stop(:dest)
-
-
-
-  # dest_index = @mta[line_sym].index(dest)
+  info[:dest_stop] = ask_stop(info[:dest_line], :dest)
 
   # The user should be told the number of stops for their trip
   display_result(info)
-
 end
 
 
 # Commit 2
 # Two lines functionality (N and L)
+# Commit 3
+# Three lines functionality (N, L and 6)
 
-def two_lines(lines)
+def multiple_lines(lines)
   info = Hash.new
-
   display_lines(lines)
 
   # The user should be asked what line they want to get on
@@ -155,7 +129,9 @@ def two_lines(lines)
   display_stops(info[:origin_line])
 
   # The user should be able to enter the stop that they want to get on at
-  info[:origin_stop] = ask_stop(:origin)
+  info[:origin_stop] = ask_stop(info[:origin_line], :origin)
+
+  display_lines(lines)
 
   # The user should be asked what line they want to get off
   info[:dest_line] = ask_line(:dest)
@@ -164,25 +140,80 @@ def two_lines(lines)
   display_stops(info[:dest_line])
 
   # The user should be able to enter the stop that they want to get off at
-  info[:dest_stop] = ask_stop(:dest)
+  info[:dest_stop] = ask_stop(info[:dest_line], :dest)
 
   # The user should be told the number of stops for their trip
   display_result(info)
 end
 
 
-# Commit 3
-# Three lines functionality (N, L and 6)
+# lines = Array.new
+# @mta.each_key do |key|
+#   lines.push(key.to_s)
+# end
+# two_lines(lines)
+# # one_line("n")
 
-def three_lines(lines) {
 
-  # Add functionality for the 6 line
-
-}
-
-lines = Array.new
-@mta.each_key do |key|
-  lines.push(key.to_s)
+def display_title
+  print "" +
+    "\n              ███╗   ███╗████████╗ █████╗     ███████╗██╗   ██╗██████╗ ██╗    ██╗ █████╗ ██╗   ██╗" +
+    "\n              ████╗ ████║╚══██╔══╝██╔══██╗    ██╔════╝██║   ██║██╔══██╗██║    ██║██╔══██╗╚██╗ ██╔╝" +
+    "\n              ██╔████╔██║   ██║   ███████║    ███████╗██║   ██║██████╔╝██║ █╗ ██║███████║ ╚████╔╝" +
+    "\n              ██║╚██╔╝██║   ██║   ██╔══██║    ╚════██║██║   ██║██╔══██╗██║███╗██║██╔══██║  ╚██╔╝" +
+    "\n              ██║ ╚═╝ ██║   ██║   ██║  ██║    ███████║╚██████╔╝██████╔╝╚███╔███╔╝██║  ██║   ██║" +
+    "\n              ╚═╝     ╚═╝   ╚═╝   ╚═╝  ╚═╝    ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝" +
+    "\n" +
+    "\n                                                                                   By Sungwon Ryu" +
+    "\n\n"
 end
-two_lines(lines)
-# one_line("n")
+
+
+def display_main_menu
+  print "" +
+    "\n1. One line functionality ( N line )" +
+    "\n2. Two lines functionality ( N and L )" +
+    "\n3. Three lines functionality (N, L and 6)" +
+    "\n4. Four lines functionality (N, L, 6 and 1)" +
+    "\n5. Quit" +
+    "\n\n"
+
+  menu = (1..5).collect { |x| x.to_s }
+
+  print "Please choose one of the above menu : "
+  user_response = gets.chomp
+  menu = (1..5).collect { |x| x.to_s }
+
+  unless menu.include?(user_response)
+    puts "\nError! You entered a wrong number!"
+    print "Please choose one of the above menu : "
+    user_response = gets.chomp
+  end
+
+  user_response
+end
+
+
+def subway
+  display_title()
+
+  begin
+    user_choice = display_main_menu()
+
+    case user_choice
+    when "1"
+      one_line("n")
+    when "2"
+      multiple_lines(["n", "l"])
+    when "3"
+      multiple_lines(["n", "l", "s"])
+    when "4"
+      multiple_lines()
+    end
+  end while user_choice != "5"
+
+  puts "\nBye!\n"
+end
+
+
+subway()
