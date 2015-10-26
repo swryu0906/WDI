34ba82617628953c63819eeb33abbd76b2b73143
Sungwon Ryu <swryu0906@gmail.com>
2015-10-26 18:42:01 -0400

    Creates layout.erb and calc.erb and modifies app.rb in w05/d01/homework/

--- w05/d01/homework/sinatra_calc/app.rb
+++ w05/d01/homework/sinatra_calc/app.rb
@@ -1,5 +1,23 @@
 class SinatraCalc < Sinatra::Base
+  get '/' do
+    redirect '/calc'
+  end
 
+  get '/calc' do
+    erb :calc
+  end
 
+  get '/calc/' do
+    erb :calc
+  end
 
+  get '/calc/*/*/*' do
+    @args = params['splat']
+    erb :result
+  end
+
+  get '/calc/*/*/*/' do
+    @args = params['splat']
+    erb :result
+  end
 end
