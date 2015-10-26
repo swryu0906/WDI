2d98a3bb023627b95c5a9b1df895cda131395593
Sungwon Ryu <swryu0906@gmail.com>
2015-10-26 19:24:33 -0400

    Adds parsing parameters feature to app.rb in w05/d01/homework/sinatra_calc/

--- w05/d01/homework/sinatra_calc/app.rb
+++ w05/d01/homework/sinatra_calc/app.rb
@@ -1,4 +1,7 @@
+
+
 class SinatraCalc < Sinatra::Base
+
   get '/' do
     redirect '/calc'
   end
@@ -12,12 +15,49 @@ class SinatraCalc < Sinatra::Base
   end
 
   get '/calc/*/*/*' do
-    @args = params['splat']
+    @operation = params['splat'][0].chomp.downcase
+    @num_1 = params['splat'][1].chomp.to_f
+    @num_2 = params['splat'][2].chomp.to_f
+    @result = parse_operation(@operation, @num_1, @num_2)
     erb :result
   end
 
   get '/calc/*/*/*/' do
-    @args = params['splat']
+    @operation = params['splat'][0].chomp.downcase
+    @num_1 = params['splat'][1].chomp.to_f
+    @num_2 = params['splat'][2].chomp.to_f
+    @result = parse_operation(@operation, @num_1, @num_2)
     erb :result
+  end
+
+
+  def parse_operation(operation, num_1, num_2)
+    case operation
+    when 'add'
+      add(num_1, num_2)
+    when 'subtract'
+      subtract(num_1, num_2)
+    when 'multiply'
+      multiply(num_1, num_2)
+    when 'divide'
+      divide(num_1, num_2)
+    else
+    end
+  end
+
+  def add(num_1, num_2)
+    num_1 + num_2
+  end
+
+  def subtract(num_1, num_2)
+    num_1 - num_2
+  end
+
+  def multiply(num_1, num_2)
+    num_1 * num_2
+  end
+
+  def divide(num_1, num_2)
+    num_1 / num_2
   end
 end
