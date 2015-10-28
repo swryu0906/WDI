63283e13a851bcb38bee12e9f8482d9e67904507
Sungwon Ryu <swryu0906@gmail.com>
2015-10-27 20:32:03 -0400

    Creates realty_schema.sql and seed.sql in w05/d02/ASSIGNMENT/hmh_realty_nfl/hmh_realty/

---
+++ w05/d02/ASSIGNMENT/hmh_realty_nfl/hmh_realty/realty_schema.sql
@@ -0,0 +1,70 @@
+
+DROP TABLE IF EXISTS apartment;
+DROP TABLE IF EXISTS office;
+DROP TABLE IF EXISTS storefront;
+
+
+-- - An Apartment should have:
+-- 	- an apartment number
+-- 	- number of bedrooms
+-- 	- number of bathrooms
+-- 	- an address
+-- 	- a tenant
+-- 	- an occupied status
+-- 	- a square footage
+-- 	- a price
+CREATE TABLE apartment (
+  id SERIAL PRIMARY KEY,
+  apt_no VARCHAR(30) NOT NULL,
+  no_of_bedrooms INTEGER,
+  no_of_bathrooms INTEGER,
+  address VARCHAR(255),
+  tenant VARCHAR(30),
+  status VARCHAR(30),
+  square_footage INTEGER,
+  price NUMERIC(15, 6)
+);
+
+
+-- - An Office should have:
+-- 	- an office number
+-- 	- a number of floors
+-- 	- a square footage
+-- 	- a number of cubicles
+-- 	- a number of bathrooms
+-- 	- an address
+-- 	- a company name
+-- 	- an occupied status
+-- 	- a price
+CREATE TABLE office (
+  id SERIAL PRIMARY KEY,
+  office_no VARCHAR(30) NOT NULL,
+  no_of_floors INTEGER,
+  no_of_subicles INTEGER,
+  no_of_bathrooms INTEGER,
+  square_footage INTEGER,
+  address VARCHAR(255),
+  company_name VARCHAR(30),
+  status VARCHAR(30),
+  price NUMERIC(15, 6)
+);
+
+
+-- - A storefront should have:
+-- 	- an address
+-- 	- an occupied status
+-- 	- a price
+-- 	- whether there is a kitchen or not
+-- 	- a square footage
+-- 	- an owner
+-- 	- whether there is outdoor seating or not
+CREATE TABLE storefront (
+  id SERIAL PRIMARY KEY,
+  address VARCHAR(255),
+  status VARCHAR(30),
+  price NUMERIC(15, 6),
+  square_footage INTEGER,
+  owner VARCHAR(30),
+  has_kitchen BOOLEAN,
+  has_outdoor_seating BOOLEAN
+);
---
+++ w05/d02/ASSIGNMENT/hmh_realty_nfl/hmh_realty/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('1A', 1, 1, '100 4th Ave New York NY 10012', 'Steve Chen', 'renting', 1500, '400000');
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('2A', 2, 1, '120 5th Ave New York NY 10012', 'Anthony Ng', 'renting', 2000, '500000');
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('1B', 3, 2, '340 6th Ave New York NY 10012', 'Luis Mitch', 'empty', 2500, '6000000');
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('2B', 4, 2, '560 7th Ave New York NY 10013', 'Jeffrey Kim', 'renting', 3000, '700000');
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('1C', 5, 3, '760 8th Ave New York NY 10013', 'Cort Fortin', 'empty', 3500, '800000');
+INSERT INTO apartment (apt_no, no_of_bedrooms, no_of_bathrooms, address, tenant, status, square_footage, price) VALUES ('2C', 6, 3, '1234 9th Ave New York NY 10013', 'Derek Lee', 'renting', 4000, '1000000');
+
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('1A', 1, 2, 2, 4000, '1542 4th st New York NY 10012', 'Google', 'renting', 2000000);
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('1B', 1, 3, 3, 5000, '1442 5th st New York NY 10013', 'Microsoft', 'renting', 3000000);
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('2A', 2, 2, 2, 4000, '1242 6th st New York NY 10014', 'Amazon', 'renting', 3000000);
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('2B', 2, 3, 3, 5000, '1342 7th st New York NY 10015', 'Facebook', 'renting', 4000000);
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('3A', 3, 2, 2, 4000, '12 8th st New York NY 10016', 'Twitter', 'renting', 4000000);
+INSERT INTO office (office_no, no_of_floors, no_of_subicles, no_of_bathrooms, square_footage, address, company_name, status, price) VALUES ('3B', 3, 3, 3, 5000, '13 9th st New York NY 10017', 'Neflex', 'renting', 5000000);
+
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('1341 4th St Brooklyn NY 13002', 'renting', '400000', 2000, 'Steve Chen', 'f', 't');
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('11 5th St Brooklyn NY 13002', 'renting','500000',  2000, 'Luis Chen', 't', 'f');
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('143 6th St Brooklyn NY 13002', 'renting','600000', 3000, 'Anthony Chen', 'f', 't');
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('133 7th St Brooklyn NY 13002', 'renting', '700000', 3000, 'Samuel Chen', 't', 'f');
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('141 8th St Brooklyn NY 13002', 'renting','800000',  4000, 'Cort Chen', 'f', 't');
+INSERT INTO storefront (address, status, price, square_footage, owner, has_kitchen, has_outdoor_seating) VALUES ('131 9th St Brooklyn NY 13002', 'renting', '900000', 4000, 'Collin Chen', 't', 'f');
