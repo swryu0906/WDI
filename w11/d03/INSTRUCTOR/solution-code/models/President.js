d546e4f3749ca43c0e343fa30a635ed6bb08a630
Syed Salahuddin <syedamin@cs.nyu.edu>
2015-12-02 13:49:47 -0500

    solution for angular http

---
+++ w11/d03/INSTRUCTOR/solution-code/models/President.js
@@ -0,0 +1,10 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var PresidentSchema = mongoose.Schema({
+	name: String,
+  start: Number,
+	end: Number
+});
+
+module.exports = mongoose.model('President', PresidentSchema);
